22950d781ad9a66abeea63eb95859165b634e359
Tamara Ilic <tamarailic11@gmail.com>
2022-08-29 12:38:21 +0200

    feat: Added ball and player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody playerRb;
     private float horizontalInput;
     private float verticalInput;
     public float speed;
+    public float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
-        
+        Move();
+        Jump();
     }
 
-    void MovePlayer()
+    void Move()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * horizontalInput * speed * Time.deltaTime);
-        transform.Translate(Vector3.right * verticalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
